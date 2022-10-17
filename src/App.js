61e5f84148821dd2cf3be19b3c24c3643a9105c2
Antti <antti.tuomola@gofore.com>
2022-10-17 13:59:28 +0300

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const editorRef = useRef(null);
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorRef.current.getContent());
+            console.log(editorRef.current.plugins.flite)
+        }
+    };
+
+    const setAntti = () => {
+        console.log(flite._userManager)
+        console.log(editorRef.current.plugins.flite)
+        flite._userManager.addUser({
+            id: 'antti',
+            name: 'Antti',
+            style: {
+                insert: {
+                    color: 'blue'
+                }
+            }
+        })
+        flite._userManager.setCurrentUser('antti')
+    }
+
+    const setLauri = () => {
+        flite._userManager.addUser({
+            id: 'lauri',
+            name: 'Lauri',
+            style: {
+                insert: {
+                    color: 'red'
+                }
+            }
+        })
+        flite._userManager.setCurrentUser('lauri')
+    }
+
+    let flite;
+
+    return (
+        <div className="container">
+            <Editor
+                tinymceScriptSrc={process.env.PUBLIC_URL + '/tinymce/tinymce.min.js'}
+                onInit={(evt, editor) => editorRef.current = editor}
+                init={{
+                    height: 500,
+                    width: 650,
+                    menubar: true,
+                    plugins: [
+                        'flite',
+                    ],
+                    toolbar: 'flite',
+                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+
+                    setup: (editor) => {
+                        editor.on('flite:init', (e) => {
+                            flite = e.flite;
+                        }
+                        )
+                    },
+                }}
+            />
+            <button onClick={log}>Log editor content</button>
+            <button onClick={setAntti}>Set Antti as user</button>
+            <button onClick={setLauri}>Set Lauri as user</button>
+
+        </div>
+    );
 }
-
-export default App;
